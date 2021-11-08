719cfcf0479155956bb782cd4f9f6bd2cedba07e
Anna Leptyuk <Leptyukanna@mail.ru>
2021-11-09 00:06:53 +0600

    Add NewsPage.cs

---
+++ MopileApps/StartPage.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+
+namespace MopileApps
+{
+    class StartPage : ContentPage
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string News { get; set; }
+        public string Autor { get; set; }
+        public string Date { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            StartPage friends = obj as StartPage;
+            return this.Id == friends.Id;
+        }
+    }
+}
